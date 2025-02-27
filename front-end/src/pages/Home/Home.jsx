1fc173569a935ff6e409db4b1456d16ccb137157
sakthi <sakthipalanisamy7@gmail.com>
2025-02-27 21:59:26 +0530

    fixed incosistency in themes in Login , Signup and Home pages

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -1,45 +1,44 @@
+// Home.jsx
+import { Button } from "@chakra-ui/react";
 import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Typed from "typed.js";
 import "./Home.css";
-import { Button } from "@chakra-ui/react";
 
 const Home = () => {
   const navigate = useNavigate();
   const typer = useRef(null);
+  
   useEffect(() => {
     const typed = new Typed(typer.current, {
       strings: ["Easy!", "Efficient!", "Effortless!"],
       typeSpeed: 100,
       backSpeed: 100,
       loop: true,
+      startDelay: 300,
+      showCursor: true,
+      cursorChar: '|',
+      autoInsertCss: true,
     });
-
     return () => {
       typed.destroy(); // Cleanup the Typed instance when the component unmounts
     };
   }, []);
+ 
   return (
     <div className="Home text-center">
       <div className="text-5xl">
-        <div>
-          <h1 className="heading font-extrabold">
-            Assignment tracking made{" "}
+        <h1 className="heading font-extrabold">
+          Assignment tracking made{" "}
+          <span className="dynamic-text-container">
             <span className="dynamic-text" ref={typer}></span>
-          </h1>
-        </div>
+          </span>
+        </h1>
         <div className="text-2xl flex justify-around mt-12">
-          <Button
-            onClick={() => navigate("/login")}
-            colorScheme="blue"
-          >
+          <Button onClick={() => navigate("/login")} colorScheme="blue" color={"white"} bg={"blue.500"}>
             Login
           </Button>
-
-          <Button
-            colorScheme="green"
-            onClick={() => navigate("/signup")}
-          >
+          <Button colorScheme="green" onClick={() => navigate("/signup")} color={"white"} bg={"green.500"}>
             Join for free
           </Button>
         </div>
